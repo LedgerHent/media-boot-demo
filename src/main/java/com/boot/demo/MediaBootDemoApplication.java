9003e1aaf15bbedb6cd32d02cfd47f5fc1dc0bdd
caozhenlong <caozhenlong@36kr.com>
2020-01-19 16:46:48 +0800

    Initial commit

---
+++ src/main/java/com/boot/demo/MediaBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaBootDemoApplication.class, args);
+    }
+
+}
